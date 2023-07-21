cdd9e3fcadf46435c9e436d5b7035f644e18f7ce
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-07-21 08:10:10 +0000

    2.7 Complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,8 +12,13 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const newNameObject = {name: newName}
-    setPersons(persons.concat(newNameObject))
+    
+    if (persons.map((person) => person.name).includes(newName)){
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newNameObject = {name: newName}
+      setPersons(persons.concat(newNameObject))
+    }
   }
 
   return (
