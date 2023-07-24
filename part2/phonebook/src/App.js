2e61095b554746c308fd58b6bff7b60344f7a199
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-07-24 11:21:29 +0000

    2.11 Complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  let nameList = [...persons];
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -45,13 +43,17 @@ const App = () => {
     setNewNumber('')
   }
 
-  let nameList = [];
+  useEffect(() => {
+    axios.get('https://balinkrish2000-friendly-goldfish-g7jwqp45756cv6xp-3002.preview.app.github.dev/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+
   if (filterName.length > 0) {
     nameList = persons.filter(
       (person) => person.name.toLowerCase().search(filterName.toLowerCase()) !== -1 ? person:null
     )
-  } else {
-    nameList = [...persons]
   }
 
   return (
@@ -67,7 +69,7 @@ const App = () => {
           onSubmit={handleSubmit}
           />
       <h2>Numbers</h2>
-      <Persons nameList={nameList}/>
+      <Persons nameList= {nameList}/>
     </div>
   )
 }
