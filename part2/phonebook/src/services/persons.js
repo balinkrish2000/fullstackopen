fa842e8b87a18dd86bdb0dcdddbf2fb319717c1a
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-08-09 16:28:33 +0000

    Updated port number in backend url

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let baseUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-    baseUrl = 'https://zany-palm-tree-g7jwqp4574pcwrjq-3000.app.github.dev/api/persons'
+    baseUrl = 'https://zany-palm-tree-g7jwqp4574pcwrjq-3001.app.github.dev/api/persons'
 } else {
     baseUrl = '/api/persons'
 }
